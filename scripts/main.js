90078caa53fc138f5b1ef111f17859e2852168c9
Jamie Gamblin <jamiemgamblin@gmail.com>
2015-05-21 17:53:49 -0400

    Saving progress

--- scripts/main.js
+++ scripts/main.js
@@ -7,10 +7,10 @@ var urlImage;
 items.results.forEach(function(item) {
 
   urlImage = item.Images[0].url_170x135;
-  resultsTitle = '<p class="resultsTitle">' + item.title + '</p>';
-  resultsShop = '<p class="resultsShop">' + item.Shop.shop_name + '</p>';
-  resultsPrice = '<p class="resultsPrice">' + item.price + '</p>';
-  resultsCurrencyCode = '<p class="resultsCurrencyCode">' + item.currency_code + '</p>';
+  resultsTitle = '<div class="resultsTitle">' + item.title + '</div>';
+  resultsShop = '<div class="resultsShop">' + item.Shop.shop_name + '</div>';
+  resultsPrice = '<div class="resultsPrice">' + item.price + '</div>';
+  resultsCurrencyCode = '<div class="resultsCurrencyCode">' + item.currency_code + '</div>';
   resultsImage = '<img src="' + urlImage + '" />';
   resultsHolder = '<li>' + resultsImage + resultsTitle + '\n' + resultsShop + ' ' + resultsPrice + ' ' + resultsCurrencyCode + '</li>';
 
