56c5d069240290f22ff8c56f7a09aa837205e148
Jamie Gamblin <jamiemgamblin@gmail.com>
2015-05-22 13:01:46 -0400

    finalizing positioning and styling

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ $(document).ready(function (){
 
 var resultsList = $('#contentResults');
 var resultsImage, resultsTitle, resultsPrice, resultsCurrencyCode, resultsShop, resultsHolder;
-var urlImage;
+var urlImage, belowTitle, priceAndCurrency;
 
 items.results.forEach(function(item) {
 
@@ -12,7 +12,9 @@ items.results.forEach(function(item) {
   resultsPrice = '<div class="resultsPrice">' + item.price + '</div>';
   resultsCurrencyCode = '<div class="resultsCurrencyCode">' + item.currency_code + '</div>';
   resultsImage = '<img src="' + urlImage + '" />';
-  resultsHolder = '<li>' + resultsImage + resultsTitle + '\n' + resultsShop + ' ' + resultsPrice + ' ' + resultsCurrencyCode + '</li>';
+  priceAndCurrency = '<div class="priceAndCurrency">' + resultsPrice + ' ' + resultsCurrencyCode + '</div>';
+  belowTitle = '<div class="belowTitle">' + resultsShop + priceAndCurrency  + '</div>';
+  resultsHolder = '<li>' + resultsImage + resultsTitle + '\n' + belowTitle + '</li>';
 
   resultsList.append(resultsHolder);
 
