f5ac6ce16df90835b3c6024c2d8e97e0f4360b8c
Jamie Gamblin <jamiemgamblin@gmail.com>
2015-05-22 13:11:57 -0400

    Added $ sign to price

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,7 @@ items.results.forEach(function(item) {
   urlImage = item.Images[0].url_170x135;
   resultsTitle = '<div class="resultsTitle">' + item.title + '</div>';
   resultsShop = '<div class="resultsShop">' + item.Shop.shop_name + '</div>';
-  resultsPrice = '<div class="resultsPrice">' + item.price + '</div>';
+  resultsPrice = '<div class="resultsPrice">' + '$' + item.price + '</div>';
   resultsCurrencyCode = '<div class="resultsCurrencyCode">' + item.currency_code + '</div>';
   resultsImage = '<img src="' + urlImage + '" />';
   priceAndCurrency = '<div class="priceAndCurrency">' + resultsPrice + ' ' + resultsCurrencyCode + '</div>';
