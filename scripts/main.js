dd8dc99a1b49895f52576737e518765fb71bba6a
Jamie Gamblin <jamiemgamblin@gmail.com>
2015-05-26 13:32:35 -0400

    placed hamburger icon in hover over images

--- scripts/main.js
+++ scripts/main.js
@@ -11,7 +11,7 @@ items.results.forEach(function(item) {
   resultsShop = '<div class="resultsShop"><a>' + item.Shop.shop_name + '</a></div>';
   resultsPrice = '<div class="resultsPrice">' + '$' + item.price + '</div>';
   resultsCurrencyCode = '<div class="resultsCurrencyCode">' + item.currency_code + '</div>';
-  resultsImage = '<a class="resultsImage"><img src="' + urlImage + '" /><i class="hoverButtons"></i><a/>';
+  resultsImage = '<a class="resultsImage"><img src="' + urlImage + '" /></a>';
   priceAndCurrency = '<div class="priceAndCurrency">' + resultsPrice + ' ' + resultsCurrencyCode + '</div>';
   belowTitle = '<div class="belowTitle">' + resultsShop + priceAndCurrency  + '</div>';
   resultsHolder = '<li>' + resultsImage + resultsTitle + '\n' + belowTitle + '</li>';
